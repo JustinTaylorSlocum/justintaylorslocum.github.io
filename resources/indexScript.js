cd86ced25359d56a1940e8a867fc0c4d0ac15e49
Justin Slocum <justintaylorslocum@gmail.com>
2019-08-19 20:24:43 -0400

    Add contact icon labels

--- resources/indexScript.js
+++ resources/indexScript.js
@@ -1,7 +1,24 @@
+var mouseOnConButton = false;
+
 window.onload = function() {
 	$('.wrapper').css('opacity', 1);
+	contactIconInteraction();
+	$('body').mousemove(function(e) {
+		if (mouseOnConButton) {
+			$('.tooltip').css('left', e.pageX - 35);
+			$('.tooltip').css('top', e.pageY - 75);
+		}
+	});
 }
 
-$('.contactIcons').hover(function() {
-	console.log("Hello there!");
-});
+function contactIconInteraction() {
+	$('.ttButton').hover(function() {
+		mouseOnConButton = true;
+		let tipName = $(this).attr("label");
+		$('.tooltip').text(tipName);
+		$('.tooltip').css('opacity', "0.8");
+	}, function() {
+		mouseOnConButton = false;
+		$('.tooltip').css('opacity', '0');
+	});
+}
