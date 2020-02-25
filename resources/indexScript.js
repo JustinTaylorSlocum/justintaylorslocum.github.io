26b8bb54085a02f2e3d0635852fb34a74b398936
Justin Slocum <justin.slocum@wne.edu>
2020-02-25 15:00:46 -0500

    Scaling for mobile

--- resources/indexScript.js
+++ resources/indexScript.js
@@ -22,3 +22,8 @@ function contactIconInteraction() {
 		$('.tooltip').css('opacity', '0');
 	});
 }
+
+var siteWidth = 1280;
+var scale = screen.width /siteWidth
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
