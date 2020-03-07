aa60e27f0b44d3986b4a63983add1e7f123f22b1
Justin Slocum <justin.slocum@wne.edu>
2020-03-07 06:37:10 -0500

    Fix Microsoft Edge/IE compatibility

--- resources/indexScript.js
+++ resources/indexScript.js
@@ -1,23 +1,26 @@
 var mouseOnConButton = false;
 
-window.onload = function() {
+$('document').ready(function() {
 	$('.wrapper').css('opacity', 1);
 	contactIconInteraction();
 	$('body').mousemove(function(e) {
 		if (mouseOnConButton) {
-			$('.tooltip').css('left', e.pageX - 35);
+			$('.tooltip').css('left', e.pageX - 47);
 			$('.tooltip').css('top', e.pageY - 75);
 		}
 	});
-}
+});
 
 function contactIconInteraction() {
-	$('.ttButton').hover(function() {
+
+	$('.ttbutton').mouseenter(function() {
 		mouseOnConButton = true;
 		let tipName = $(this).attr("label");
 		$('.tooltip').text(tipName);
 		$('.tooltip').css('opacity', "0.8");
-	}, function() {
+	});
+
+	$('.ttbutton').mouseleave(function() {
 		mouseOnConButton = false;
 		$('.tooltip').css('opacity', '0');
 	});
--- resources/skillsTextInteractive.js
+++ resources/skillsTextInteractive.js
@@ -1,14 +1,14 @@
-var skills = ["Java Developer.", "JavaScript Developer.", "React Developer.", "Agile Developer."];
-var readyToWrite = true;
-
-// setInterval(function() {
-// 	var skillText = $('.main p').html();
-// 	if (skillText.charAt(skillText.length - 1) == "|") {
-// 		$('.main p').html(skillText.substring(0, skillText.length - 1));
-// 	} else {
-// 		$('.main p').html(skillText + "|");
-// 	}
-// }, 500);
+var pageMessage = "A software engineer with experience in both Front-end and Back-end technologies.";
+setInterval(function() {
+	if ($('.main p').html().substring(0, pageMessage.length).length == pageMessage.length) {
+	var skillText = $('.main p').html();
+	if (skillText.charAt(skillText.length - 1) == "|") {
+		$('.main p').html(skillText.substring(0, skillText.length - 1));
+	} else {
+		$('.main p').html(skillText + "|");
+	}
+	}
+}, 500);
 
 function timePromise() {
 	return new Promise(resolve => {setTimeout(resolve, 50)});
@@ -20,47 +20,8 @@ async function writeToDisplay(word) {
 		var skillText = $('.main p').html();
 		$('.main p').html(skillText + word[i]);
 	}
-	readyToWrite = false;
 }
 
 $('document').ready(function() {
-	var word = skills[Math.floor(Math.random() * skills.length)];
-	writeToDisplay("A software engineer with experience in both Front-end and Back-end technologies.");
+	writeToDisplay(pageMessage);
 });
-// async function eraseSkillDisplayCharacterPromise() {
-// 	await timePromise();
-// 	var skillText = $('.main p').html();
-// 	$('.main p').html(skillText.substring(0, skillText.length - 1));
-// 	console.log($('.main p').html());
-// }
-
-// async function eraseSkillDisplay() {
-// 	while ($('.main p').html() != "-") {
-// 		await timePromise();
-// 		var skillText = $('.main p').html();
-// 		$('.main p').html(skillText.substring(0, skillText.length - 1));
-// 	}
-// 	readyToWrite = true;
-// }
-
-// async function writeToDisplay(word) {
-// 	//eraseSkillDisplay();
-// 	for (var i = 0; i < word.length; i++) {
-// 		await timePromise();
-// 		var skillText = $('.main p').html();
-// 		$('.main p').html(skillText + word[i]);
-// 	}
-// 	readyToWrite = false;
-// }
-
-
-// setInterval(function() {
-// 	if (document.hasFocus()) {
-// 		if (readyToWrite) {
-// 			var word = skills[Math.floor(Math.random() * skills.length)];
-// 			writeToDisplay(word);
-// 		} else {
-// 			eraseSkillDisplay();
-// 		}
-// 	}
-// }, 3000)
